8570b03a7a7671e3a51df703d91ca250d0544432
Matheus Grigorio Lins Medeiros <Mattuez@academicos.com>
2024-03-12 20:40:00 -0300

    added jakarta validation

--- src/main/java/com/matheus/servicex/api/model/categoria/CategoriaInputDto.java
+++ src/main/java/com/matheus/servicex/api/model/categoria/CategoriaInputDto.java
@@ -1,5 +1,6 @@
 package com.matheus.servicex.api.model.categoria;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +8,6 @@ import lombok.Setter;
 @Setter
 public class CategoriaInputDto {
 
+    @NotEmpty
     private String nome;
 }
